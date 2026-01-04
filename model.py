fc43f6d4f1e8fbe941b90fb8929a263c65680ff3
ZECT <suj01@mail.ru>
2026-01-04 12:23:02 +0700

    Update model.py

--- model.py
+++ model.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("Kvartirs/data/main_train.csv")
 
-print(df["city"].value_counts())
+city_num = {
+    'Moscow': 1,
+    'Novosibirsk': 2,
+    'Kazan': 3,
+    'Petersburg': 4,
+    'Yekaterinburg': 5
+}
+
+df['city'] = df['city'].map(city_num)
+
+print(df['city'])
